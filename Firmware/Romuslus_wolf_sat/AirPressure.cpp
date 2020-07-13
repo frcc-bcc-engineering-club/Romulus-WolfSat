5e98c5acc531f8b693b226def45e811e93502074
FRCCSour137 <47700245+FRCCSour137@users.noreply.github.com>
2020-07-12 19:03:08 -0600

    AirPressure, not integrated with main yet

--- Firmware/Romuslus_wolf_sat/AirPressure.cpp
+++ Firmware/Romuslus_wolf_sat/AirPressure.cpp
@@ -1,1 +1,15 @@
+#include "AirPressure.h"
+
+void AirPressure::FillData()
+{
+  dataSet[0] = ms5803.getPressure(ADC_4096);
+  dataSet[1] = ms5803.getTemperature(CELSIUS, ADC_4096);
+  dataSet[2] = ms5803.getTemperature(FAHRENHEIT, ADC_4096);
+  altimeter = Altitude(dataSet[0], baseline);
+}
+
+double AirPressure::Altitude(double pressure, double base)
+{
+  return (44330.0 * (1 - pow(pressure / base, 1 / 5.255)));
+}
 
--- Firmware/Romuslus_wolf_sat/AirPressure.h
+++ Firmware/Romuslus_wolf_sat/AirPressure.h
@@ -1,1 +1,26 @@
+#include "Sensor.h"
+#include <Wire.h>
+#include <SparkFun_MS5803_I2C.h>
 
+#ifndef AIRPRESSURE_H
+#define AIRPRESSURE_H
+
+
+class AirPressure : public Sensor
+{
+  MS5803 ms5803 = MS5803(ADDRESS_HIGH);
+  double baseline = 0;
+  double altimeter = 0;
+  double Altitude(double , double);
+  public:
+  AirPressure() : Sensor(3)
+  {
+    ms5803.reset();
+    ms5803.begin();
+    baseline = ms5803.getPressure(ADC_4096);
+    altimeter = baseline;
+  }
+  void FillData() override;
+};
+
+#endif
