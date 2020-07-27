0b3826fce2061e562b07fae2b039813cacd0be82
FRCCSour137 <47700245+FRCCSour137@users.noreply.github.com>
2020-07-27 14:04:44 -0600

    Limon and Deer Trail elevations added to Air Pressure

--- Firmware/Romuslus_wolf_sat/AirPressure.cpp
+++ Firmware/Romuslus_wolf_sat/AirPressure.cpp
@@ -1,5 +1,5 @@
 #include "AirPressure.h"
-const double BASEALTITUDE = 1539.85; // berthoud. 1476.76 for Eaton. In meters.
+const double BASEALTITUDE = 1539.85; // berthoud. 1641.35 for Limon. 1586.18 for Deer Trail
 
 void AirPressure::FillData()
 {
